a0bb9e0121b3174d8d53bd67782acf2dbc9ecbd0
rahullrawat <rahulsinghrawat760@gmail.com>
2024-08-06 15:45:22 +0530

    refactor: fix getURLFromMedia util function

--- src/payload/utils.ts
+++ src/payload/utils.ts
@@ -3,6 +3,8 @@ import { Media } from './types'
 export function getURLFromMedia(media: Media | string) {
   if (typeof media === 'string') {
     return media
+  } else if (media && typeof media.url === 'string') {
+    return media.url
   }
-  return media.url!
+  return ''
 }
